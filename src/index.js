0617b41ceca0cf69501292de80b422369ec8faf0
Vitaliy <flatronsamatron@gmail.com>
2020-02-25 10:53:16 +0200

    init: start typical-array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array == null ? 0 : array.length == 0 ? 0 : Math.min(...array)
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array == null ? 0 : array.length == 0 ? 0 :  Math.max(...array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  if(array == null){ return 0}
+  else if(array.length == 0){ return 0}
+  else{
+    let arr = array.reduce((a,b)=>{
+      return a+b
+    })
+  
+    return arr/array.length
+  }
+
 }
